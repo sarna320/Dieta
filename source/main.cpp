b3c0c597158f59c7be46a8292bb046d6c9d0592d
sarna320 <sarnacki99@wp.pl>
2022-12-29 23:44:27 +0100

    dodano print osoby

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void getDataPerson(int &w, char &p, int &wi, int &wz);
+void printPerson(Person per);
 
 int main()
 {
@@ -11,21 +12,9 @@ int main()
     char plec;
     int wiek;
     int wzrost;
-    getDataPerson(waga,plec,wiek,wzrost);
-
+    getDataPerson(waga, plec, wiek, wzrost);
     Person osoba(waga, plec, wiek, wzrost);
-    osoba.calculate_caloric_needs();
-    cout << "Twoje dane:" << endl;
-    cout << "Waga:" << osoba.get_weight() << endl;
-    cout << "Wzrost: " << osoba.get_height() << endl;
-    cout << "Plec: " << osoba.get_sex() << endl;
-    cout << "Wiek: " << osoba.get_age() << endl
-         << endl;
-
-    cout << "Zapotrzebowanie kaloryczne: " << osoba.get_caloric_needs() << endl;
-    cout << "Wegle: " << osoba.get_carbohydrates() << endl;
-    cout << "Tluszcze: " << osoba.get_fat() << endl;
-    cout << "Bialko: " << osoba.get_protein() << endl;
+    printPerson(osoba);
 
     return 0;
 }
@@ -35,10 +24,27 @@ void getDataPerson(int &w, char &p, int &wi, int &wz)
 
     cout << "Podaj wage" << endl;
     cin >> w;
-    cout << "Podaj plec" << endl;
+    cout << "Podaj plec m lub k" << endl;
     cin >> p;
     cout << "Podaj wiek" << endl;
     cin >> wi;
     cout << "Podaj wzrost" << endl;
     cin >> wz;
+    cout<<endl;
+}
+
+void printPerson(Person per)
+{
+    per.calculate_caloric_needs();
+    cout << "Twoje dane:" << endl;
+    cout << "Waga:" << per.get_weight() << endl;
+    cout << "Wzrost: " << per.get_height() << endl;
+    cout << "Plec: " << per.get_sex() << endl;
+    cout << "Wiek: " << per.get_age() << endl
+         << endl;
+
+    cout << "Zapotrzebowanie kaloryczne: " << per.get_caloric_needs() << endl;
+    cout << "Wegle: " << per.get_carbohydrates() << endl;
+    cout << "Tluszcze: " << per.get_fat() << endl;
+    cout << "Bialko: " << per.get_protein() << endl;
 }
